40e9f489cb4a7650db40b6ec64d30f99971440d8
Janis Bergs <janis.bergs@skry.se>
2024-09-15 09:52:21 +0200

    Initial Commit

---
+++ MarshallCrypto/MarshallCryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarshallCryptoApp.swift
+//  MarshallCrypto
+//
+//  Created by Janis Bergs on 2024-09-15.
+//
+
+import SwiftUI
+
+@main
+struct MarshallCryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MarshallCryptoTests/MarshallCryptoTests.swift
@@ -0,0 +1,17 @@
+//
+//  MarshallCryptoTests.swift
+//  MarshallCryptoTests
+//
+//  Created by Janis Bergs on 2024-09-15.
+//
+
+import Testing
+@testable import MarshallCrypto
+
+struct MarshallCryptoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
